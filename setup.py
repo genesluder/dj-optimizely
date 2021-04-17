ebf2d1a323f20d4919c00b1647274539987c2b38
Gene Sluder <me@genesluder.com>
2021-04-17 00:03:30 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='dj-optimizely',
-    version='0.0.3',
+    version='0.0.4',
     author='Gene Sluder',
     author_email='gene@gobiko.com',
     description='Store Optimizely datafile in a Django model',
